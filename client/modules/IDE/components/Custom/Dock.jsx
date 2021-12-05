cae4a7dea7e1546690b2995a564869121c543beb
Huaigu Lin <kevinlinhg@outlook.com>
2021-12-05 03:33:46 -0500

    set tool bar layout and add icons

--- client/modules/IDE/components/Custom/Dock.jsx
+++ client/modules/IDE/components/Custom/Dock.jsx
@@ -3,21 +3,34 @@ import { styled } from '@mui/material/styles';
 import {
   AppBar,
   Box,
+  Grid,
   IconButton,
   Menu,
   MenuItem,
   Toolbar
 } from '@mui/material';
-import { Brush as BrushIcon } from '@mui/icons-material';
+import BrushIcon from '@mui/icons-material/Brush';
+import CropSquareIcon from '@mui/icons-material/CropSquare';
 
 export default function Dock() {
   return (
-    <AppBar position="sticky">
-      <Toolbar>
-        <IconButton size="large" edge="start">
-          <BrushIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky" color="transparent">
+        <Toolbar>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <IconButton size="large" edge="start" color="secondary">
+                <BrushIcon fontSize="large" />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <IconButton size="large" edge="start">
+                <CropSquareIcon fontSize="large" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
